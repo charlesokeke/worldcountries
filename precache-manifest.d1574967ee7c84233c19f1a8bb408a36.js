82ec53897cdebd0a2614ef68312ec56a6b82c939
charles <okekeemeka20@gmail.com>
2019-09-23 19:47:35 -0400

    Updates

---
+++ precache-manifest.d1574967ee7c84233c19f1a8bb408a36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e278d4b46e02fcf2e682de9b5f13df0",
+    "url": "/worldcountries/index.html"
+  },
+  {
+    "revision": "705a546134accac4fc58",
+    "url": "/worldcountries/static/css/main.0fe94b75.chunk.css"
+  },
+  {
+    "revision": "b823651e517e50dc1aca",
+    "url": "/worldcountries/static/js/2.b1822453.chunk.js"
+  },
+  {
+    "revision": "705a546134accac4fc58",
+    "url": "/worldcountries/static/js/main.2bdc56e2.chunk.js"
+  },
+  {
+    "revision": "75b5f8e0c63123d9ded5",
+    "url": "/worldcountries/static/js/runtime-main.a90ddb51.js"
+  }
+]);
