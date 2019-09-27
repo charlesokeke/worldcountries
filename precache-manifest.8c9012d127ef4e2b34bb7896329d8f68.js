530ef86de9fa6763eef8e6e85c69f6c84b5e2868
charles <okekeemeka20@gmail.com>
2019-09-27 13:46:23 -0400

    Updates

---
+++ precache-manifest.8c9012d127ef4e2b34bb7896329d8f68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93424f93cd8e1f9d5d5b1f5c71e3ba5e",
+    "url": "/worldcountries/index.html"
+  },
+  {
+    "revision": "2e88df99dfbd0c093230",
+    "url": "/worldcountries/static/css/main.f3369bd1.chunk.css"
+  },
+  {
+    "revision": "b3b8f53c1c629bf8d8a2",
+    "url": "/worldcountries/static/js/2.6e0a6e4c.chunk.js"
+  },
+  {
+    "revision": "2e88df99dfbd0c093230",
+    "url": "/worldcountries/static/js/main.f251290a.chunk.js"
+  },
+  {
+    "revision": "75b5f8e0c63123d9ded5",
+    "url": "/worldcountries/static/js/runtime-main.a90ddb51.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/worldcountries/precache-manifest.d1574967ee7c84233c19f1a8bb408a36.js"
+  "/worldcountries/precache-manifest.8c9012d127ef4e2b34bb7896329d8f68.js"
 );
 
 self.addEventListener('message', (event) => {
